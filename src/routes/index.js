54582e663270c12258d9b2b938bdc591d1765dda
Krisna <krisnaerlangga08@gmail.com>
2025-12-11 21:00:18 +0800

    feat(history): add history route

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,4 +17,23 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+/**
+ * Get historical trends
+ * GET /history
+ */
+router.get("/history", async (req, res, next) => {
+  try {
+    const { getAllCountries } = require("../utils/db-helpers");
+    const countries = await getAllCountries();
+
+    res.render("history", {
+      title: "Historical Trends",
+      active: "history",
+      countries,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
